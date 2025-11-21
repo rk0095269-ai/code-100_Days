a6e3d3ef7349db27b272e93b4a224448f3ede1d3
Rupesh Kumar <rk0095269@gmail.com>
2025-11-21 17:16:43 +0530

    Update Question_02.c

--- Question_02.c
+++ Question_02.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-
 int main() {
-    int a,b,c;
-
+    int a;
     printf("Enter first number: ");
     scanf("%d", &a);
-
+    int b;
     printf("Enter second number: ");
     scanf("%d", &b);
 
    int  sum = a+b;
    int difference = a-b;
-   int product =a*b;
-
-    printf("Sum = %d\n", sum);
-    printf("Difference = %d\n", difference);
-    printf("Product = %d\n", product);
+    int product =a*b;
+   
+    printf(" Sum = %d\n", sum);
+    printf(" Difference = %d\n", difference);
+    printf(" Product = %d\n", product);
 
     return 0;
 }
