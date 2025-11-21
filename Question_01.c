a8794ba12cfb393d01c3bbcce8c7d13dbbbd37ca
Rupesh Kumar <rk0095269@gmail.com>
2025-11-21 16:54:35 +0530

    Update Question_01.c

--- Question_01.c
+++ Question_01.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a, b ,sum;
+    int x,y;
     
     printf("Enter first number: ");
-    scanf("%d", &a);
+    scanf("%d", &x);
     
     printf("Enter second number: ");
-    scanf("%d", &b);
-    sum = a + b ;
+    scanf("%d", &y);
+    int sum =x+y;
     printf("The sum of 1st and 2nd number is %d",sum);
     
     return 0;
