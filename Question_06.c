c914998ebfe6ca7566af024911b7e29a5d231a77
Rupesh Kumar <rk0095269@gmail.com>
2025-11-21 20:18:40 +0530

    Update Question_06.c

--- Question_06.c
+++ Question_06.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, temp;
+    int a,b;
 
     
     printf("Enter first number: ");
@@ -11,13 +11,11 @@ int main() {
     scanf("%d", &b);
 
    
-    printf("\nBefore swapping: a = %d, b = %d\n", a, b);
-
+    printf("\nBefore swapping: a = %d, b = %d\n", a,b);
     
-    temp = a;
-    a = b;
-    b = temp;
-
+    a=a+b;
+    b=a-b;
+    a=a-b;
     printf("After swapping:  a = %d, b = %d\n", a, b);
 
     return 0;
