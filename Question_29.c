a33bcd028b796af79ff1cf68288a4b98e6683482
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 16:15:58 +0530

    Update Question_29.c

--- Question_29.c
+++ Question_29.c
@@ -1,22 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int n;
-    unsigned long long factorial = 1;  
-
-   
-    printf("Enter a non-negative integer: ");
+    int n, i;
+    int fact = 1;
+    
+    printf("Enter number: ");
     scanf("%d", &n);
-
-    if(n < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-    } else {
-        
-        for(int i = 1; i <= n; i++) {
-            factorial *= i;
-        }
-        printf("Factorial of %d = %llu\n", n, factorial);
+    
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
     }
-
+    
+    printf("Factorial = %d\n", fact);
+    
     return 0;
 }
