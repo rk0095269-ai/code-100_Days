fc24de5c15ba9e6b6a210f3a592556325b2dd145
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 17:13:54 +0530

    Update Question_40.c

--- Question_40.c
+++ Question_40.c
@@ -1,27 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    char binary[32];
-
+    int num, i;
+    int binary[32];
     
-    printf("Enter a binary number: ");
-    scanf("%s", binary);
-
+    printf("Enter number: ");
+    scanf("%d", &num);
+    
+    i = 0;
+    while(num > 0) {
+        binary[i] = num % 2;
+        num = num / 2;
+        i = i + 1;
+    }
+    
     printf("1's complement: ");
-
     
-    for (int i = 0; binary[i] != '\0'; i++) {
-        if (binary[i] == '0') {
-            printf("1");
-        } else if (binary[i] == '1') {
+    for(int j = i-1; j >= 0; j--) {
+        if(binary[j] == 1) {
             printf("0");
         } else {
-           
-            printf("\nInvalid binary number.\n");
-            return 1;
+            printf("1");
         }
     }
-
-    printf("\n");
+    
     return 0;
 }
