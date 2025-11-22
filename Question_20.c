90d6c321d9214c05dfa467a50c6eb77d1959e38c
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 15:16:05 +0530

    Update Question_20.c

--- Question_20.c
+++ Question_20.c
@@ -1,36 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int day;
-
-    printf("Enter a number (1-7) for the day of the week: ");
-    scanf("%d", &day);
-
-    switch(day) {
-        case 1:
-            printf("Sunday\n");
-            break;
-        case 2:
-            printf("Monday\n");
-            break;
-        case 3:
-            printf("Tuesday\n");
-            break;
-        case 4:
-            printf("Wednesday\n");
-            break;
-        case 5:
-            printf("Thursday\n");
-            break;
-        case 6:
-            printf("Friday\n");
-            break;
-        case 7:
-            printf("Saturday\n");
-            break;
-        default:
-            printf("Invalid input! Please enter a number between 1 and 7.\n");
+    int d;
+    
+    printf("Enter day number: ");
+    scanf("%d", &d);
+    
+    switch(d) {
+        case 1: printf("Sunday\n"); break;
+        case 2: printf("Monday\n"); break;
+        case 3: printf("Tuesday\n"); break;
+        case 4: printf("Wednesday\n"); break;
+        case 5: printf("Thursday\n"); break;
+        case 6: printf("Friday\n"); break;
+        case 7: printf("Saturday\n"); break;
+        default: printf("Invalid\n");
     }
-
+    
     return 0;
 }
