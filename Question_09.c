673a249dc2e914db300c17b4b7e09ce1f05250c6
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 14:23:56 +0530

    Update Question_09.c

--- Question_09.c
+++ Question_09.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    double principal, rate, time;
-    double simpleInterest, compoundInterest, amount;
-    int i;
-
-    // Input principal, rate, and time
-    printf("Enter principal amount: ");
-    scanf("%lf", &principal);
-
-    printf("Enter rate of interest (in %%): ");
-    scanf("%lf", &rate);
-
-    printf("Enter time (in years): ");
-    scanf("%lf", &time);
-
-    simpleInterest = (principal * rate * time) / 100;
-
-   
-    amount = principal;
-    for (i = 1; i <= (int)time; i++) {
-        amount = amount + (amount * rate / 100);
-    }
-    compoundInterest = amount - principal;
-
+    double p, r, t;
+    double si;
     
-    printf("Simple Interest = %.2lf\n", simpleInterest);
-    printf("Compound Interest = %.2lf\n", compoundInterest);
+    printf("Enter principal amount: ");
+    scanf("%lf", &p);
+    
+    printf("Enter rate of interest: ");
+    scanf("%lf", &r);
+    
+    printf("Enter time in years: ");
+    scanf("%lf", &t);
+    
+    // Simple interest
+    si = (p * r * t) / 100;
 
+    printf("Simple Interest = %.2lf\n", si);
+   
     return 0;
 }
