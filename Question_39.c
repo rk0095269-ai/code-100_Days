16e3d0a53b5454686f2a60b067350c5f7fc00bb5
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 17:10:54 +0530

    Update Question_39.c

--- Question_39.c
+++ Question_39.c
@@ -1,30 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int n, digit;
-    long long product = 1; 
-    int hasOdd = 0; 
-   
-    printf("Enter a number: ");
+    int n, digit, product = 1;
+    
+    printf("Enter number: ");
     scanf("%d", &n);
-
-    int temp = n;
-
-   
-    while (temp != 0) {
-        digit = temp % 10;
-        if (digit % 2 != 0) { 
-            product *= digit;
-            hasOdd = 1; 
+    
+    while(n > 0) {
+        digit = n % 10;
+        if(digit % 2 == 1) {
+            product = product * digit;
         }
-        temp /= 10;
+        n = n / 10;
     }
-
-    if (hasOdd) {
-        printf("Product of odd digits of %d is %lld\n", n, product);
-    } else {
-        printf("No odd digits in %d\n", n);
-    }
-
+    
+    printf("Product of odd digits = %d\n", product);
+    
     return 0;
 }
