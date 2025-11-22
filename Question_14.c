5bb781ed73924f39c31549b5a4748f9217b63e84
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 14:45:55 +0530

    Update Question_14.c

--- Question_14.c
+++ Question_14.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
     char ch;
-
-   
+    
     printf("Enter a character: ");
     scanf("%c", &ch);
-
-    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-        printf("%c is a vowel.\n", ch);
+    
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        printf("%c is vowel\n", ch);
+    } else if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("%c is vowel\n", ch);
     } else {
-        printf("%c is a consonant.\n", ch);
+        printf("%c is consonant\n", ch);
     }
-
+    
     return 0;
 }
