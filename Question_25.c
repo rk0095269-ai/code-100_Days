edda05bb59184cc8f53a99e58f4712ba6ab08eed
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 15:53:04 +0530

    Update Question_25.c

--- Question_25.c
+++ Question_25.c
@@ -1,42 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    char operator;
-    double num1, num2;
-    printf("Enter first number: ");
-    scanf("%lf", &num1);
+    int a,b;
+    char op;
     
-    printf("Enter second number: ");
-    scanf("%lf", &num2);
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
     
-    printf("Enter operator (+, -, *, /, %%): ");
-    scanf(" %c", &operator);  
+    printf("Enter operator: ");
+    scanf("%c", &op);
     
-    switch(operator) {
-        case '+':
-            printf("Result: %.2lf\n", num1 + num2);
-            break;
-        case '-':
-            printf("Result: %.2lf\n", num1 - num2);
-            break;
-        case '*':
-            printf("Result: %.2lf\n", num1 * num2);
-            break;
-        case '/':
-            if(num2 != 0)
-                printf("Result: %.2lf\n", num1 / num2);
-            else
-                printf("Error: Division by zero!\n");
-            break;
-        case '%':
-            if((int)num2 != 0)
-                printf("Result: %d\n", (int)num1 % (int)num2);
-            else
-                printf("Error: Division by zero!\n");
-            break;
-        default:
-            printf("Invalid operator!\n");
+    switch(op) {
+        case '+': printf("Result = %d\n", a+b); break;
+        case '-': printf("Result = %d\n", a-b); break;
+        case '*': printf("Result = %d\n", a*b); break;
+        case '/': printf("Result = %d\n", a/b); break;
+        default: printf("Invalid operator\n");
     }
-
+    
     return 0;
 }
