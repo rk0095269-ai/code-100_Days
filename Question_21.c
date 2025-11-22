b5cbce995358e41c839e53380d34653b6468d501
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 15:22:03 +0530

    Update Question_21.c

--- Question_21.c
+++ Question_21.c
@@ -1,51 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int month;
-
-    printf("Enter month number (1-12): ");
-    scanf("%d", &month);
-
-    switch(month) {
-        case 1:
-            printf("January - 31 days\n");
-            break;
-        case 2:
-            printf("February - 28 or 29 days\n");
-            break;
-        case 3:
-            printf("March - 31 days\n");
-            break;
-        case 4:
-            printf("April - 30 days\n");
-            break;
-        case 5:
-            printf("May - 31 days\n");
-            break;
-        case 6:
-            printf("June - 30 days\n");
-            break;
-        case 7:
-            printf("July - 31 days\n");
-            break;
-        case 8:
-            printf("August - 31 days\n");
-            break;
-        case 9:
-            printf("September - 30 days\n");
-            break;
-        case 10:
-            printf("October - 31 days\n");
-            break;
-        case 11:
-            printf("November - 30 days\n");
-            break;
-        case 12:
-            printf("December - 31 days\n");
-            break;
-        default:
-            printf("Invalid month number! Please enter a number between 1 and 12.\n");
+    int m;
+    
+    printf("Enter month no: ");
+    scanf("%d", &m);
+    
+    switch(m) {
+        case 1: printf("Jan 31 days\n"); break;
+        case 2: printf("Feb 28 days\n"); break;
+        case 3: printf("Mar 31 days\n"); break;
+        case 4: printf("Apr 30 days\n"); break;
+        case 5: printf("May 31 days\n"); break;
+        case 6: printf("Jun 30 days\n"); break;
+        case 7: printf("Jul 31 days\n"); break;
+        case 8: printf("Aug 31 days\n"); break;
+        case 9: printf("Sep 30 days\n"); break;
+        case 10: printf("Oct 31 days\n"); break;
+        case 11: printf("Nov 30 days\n"); break;
+        case 12: printf("Dec 31 days\n"); break;
+        default: printf("Wrong number\n");
     }
-
-    return 0;
+    return 0;  
 }
