4b6988ede74fa77a3926bcb2a27205fe7e3d956e
Rupesh Kumar <rk0095269@gmail.com>
2025-11-22 15:28:53 +0530

    Update Question_22.c

--- Question_22.c
+++ Question_22.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main() {
-    float costPrice, sellingPrice, profit, loss;
-    float profitPercentage, lossPercentage;
-
+    float cp, sp, p, l, per;
     
-    printf("Enter Cost Price: ");
-    scanf("%f", &costPrice);
-
-    printf("Enter Selling Price: ");
-    scanf("%f", &sellingPrice);
-
-   
-    if(sellingPrice > costPrice) {
-        profit = sellingPrice - costPrice;
-        profitPercentage = (profit / costPrice) * 100;
-        printf("Profit = %.2f\n", profit);
-        printf("Profit Percentage = %.2f%%\n", profitPercentage);
+    printf("Enter cost price: ");
+    scanf("%f", &cp);
+    
+    printf("Enter selling price: ");
+    scanf("%f", &sp);
+    
+    if(sp > cp) {
+        p = sp - cp;
+        per = (p/cp)*100;
+        printf("Profit = %.2f\n", p);
+        printf("Profit percentage = %.2f\n", per);
     }
-    else if(costPrice > sellingPrice) {
-        loss = costPrice - sellingPrice;
-        lossPercentage = (loss / costPrice) * 100;
-        printf("Loss = %.2f\n", loss);
-        printf("Loss Percentage = %.2f%%\n", lossPercentage);
+    else if(cp > sp) {
+        l = cp - sp;
+        per = (l/cp)*100;
+        printf("Loss = %.2f\n", l);
+        printf("Loss percentage = %.2f\n", per);
     }
     else {
-        printf("No Profit No Loss.\n");
+        printf("No profit no loss\n");
     }
-
-    return 0;
+    return 0; 
 }
