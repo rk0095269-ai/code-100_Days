c95a07c49b966e7a56a48c368cefcfc99108a313
Rupesh Kumar <rk0095269@gmail.com>
2025-11-23 11:08:25 +0530

    Update Question_41.c

--- Question_41.c
+++ Question_41.c
@@ -7,21 +7,17 @@ int main() {
 
     temp = num;
 
-    // get last digit
     lastDigit = temp % 10;
 
-    // find first digit and find its place value
     while (temp >= 10) {
         temp = temp / 10;
         power = power * 10;
     }
     firstDigit = temp;
 
-    // remove first and last digit to get the middle part
     int middle = num % power;
     middle = middle / 10;
 
-    // build final number
     int swapped = lastDigit * power + middle * 10 + firstDigit;
 
     printf("Number after swapping first and last digit: %d\n", swapped);
