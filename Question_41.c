2dc559b159d3c649ff3afa6cac46c60ef862e00d
Rupesh Kumar <rk0095269@gmail.com>
2025-11-23 11:07:33 +0530

    Update Question_41.c

--- Question_41.c
+++ Question_41.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main() {
-    int num, n, firstDigit, lastDigit, swappedNum, place = 1, middlePart;
+    int num, temp, firstDigit, lastDigit, power = 1;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    n = num;
-    lastDigit = n % 10; 
+    temp = num;
 
-    while (n >= 10) {
-        n = n / 10;
-        place = place * 10;
+    // get last digit
+    lastDigit = temp % 10;
+
+    // find first digit and find its place value
+    while (temp >= 10) {
+        temp = temp / 10;
+        power = power * 10;
     }
-    firstDigit = n;
+    firstDigit = temp;
 
-    middlePart = num % place;     
-    middlePart = middlePart / 10;
-   
-    swappedNum = lastDigit * place + middlePart * 10 + firstDigit;
+    // remove first and last digit to get the middle part
+    int middle = num % power;
+    middle = middle / 10;
 
-    printf("Number after swapping first and last digit: %d\n", swappedNum);
+    // build final number
+    int swapped = lastDigit * power + middle * 10 + firstDigit;
+
+    printf("Number after swapping first and last digit: %d\n", swapped);
 
     return 0;
 }
