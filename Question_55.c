c464dff74d7d522cb6e38027464de23af42bbc7a
Rupesh Kumar <rk0095269@gmail.com>
2025-11-24 21:11:31 +0530

    Update Question_55.c

--- Question_55.c
+++ Question_55.c
@@ -3,26 +3,25 @@
 int main() {
     int n, i, j, isPrime;
     
-    printf("Enter the value of n: ");
+    printf("Enter a number: ");
     scanf("%d", &n);
-
+    
     printf("Prime numbers from 1 to %d are:\n", n);
-
-    for (i = 2; i <= n; i++) {  
-        isPrime = 1; 
-
-        for (j = 2; j * j <= i; j++) {
-            if (i % j == 0) {
-                isPrime = 0; 
+    
+    for(i = 2; i <= n; i++) {
+        isPrime = 1;
+        
+        for(j = 2; j <= i/2; j++) {
+            if(i % j == 0) {
+                isPrime = 0;
                 break;
             }
         }
-
-        if (isPrime) {
+        
+        if(isPrime == 1) {
             printf("%d ", i);
         }
     }
-
-    printf("\n");
+    
     return 0;
 }
