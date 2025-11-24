6b0609f71d469210019471921223d99169142bbe
Rupesh Kumar <rk0095269@gmail.com>
2025-11-24 21:06:48 +0530

    Update Question_53.c

--- Question_53.c
+++ Question_53.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int n = 5; 
+    int rows[] = {1, 3, 5, 7, 9, 7, 5, 3, 1};
     int i, j;
-
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= 2*i - 1; j++) {
+    
+    for(i = 0; i < 9; i++) {
+        for(j = 0; j < rows[i]; j++) {
             printf("*");
         }
         printf("\n");
     }
-
-    for (i = n-1; i >= 1; i--) {
-        for (j = 1; j <= 2*i - 1; j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-
+    
     return 0;
 }
