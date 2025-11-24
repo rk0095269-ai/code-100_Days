90e87661270e5e2634768c094461b3eac0c203d6
Rupesh Kumar <rk0095269@gmail.com>
2025-11-24 16:47:38 +0530

    Update Question_51.c

--- Question_51.c
+++ Question_51.c
@@ -1,20 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int i, j, n = 5, start;
-
-    for (i = n; i >= 1; i--) {
-        
-        for (j = 1; j < i; j++) {
+void main() {
+    int n, i, j;
+    
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    printf("\n");
+    
+    for(i = n; i >= 1; i--) {
+    
+        for(j = 1; j < i; j++) {
             printf(" ");
         }
 
-        for (j = i; j <= n; j++) {
+        for(j = i; j <= n; j++) {
             printf("%d", j);
         }
-
-        printf("\n"); 
+        
+        printf("\n");
     }
-
-    return 0;
+    
+    printf("\nProgram ended successfully!\n");
+    printf("Press Enter...");
+    fflush(stdin);
+    getchar();
 }
