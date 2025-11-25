30ffd5199c3c5bc102d767d9ca3e082bb74b48d9
Rupesh Kumar <rk0095269@gmail.com>
2025-11-25 22:08:39 +0530

    Update Question_67.c

--- Question_67.c
+++ Question_67.c
@@ -1,26 +1,29 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int arr[100] = {1, 2, 3, 4, 5};
-    int n = 5;
-    int x, pos, i;
-    
-    printf("Array: ");
-    for (i = 0; i < n; i++) printf("%d ", arr[i]);         //  Insert an element in an array at a given position.
+int main()
+{
+    int arr[10] = {10,20,30,40,50,60};
+    int n=6;
+    int pos=3, ele=25;
+    int i;
 
-    
-    printf("\nEnter element and position: ");
-    scanf("%d %d", &x, &pos);
-    
-    for (i = n; i > pos; i--) {
+    printf("Before: ");
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+
+    //shift elements to right
+    for(i=n;i>pos;i--)
+    {
         arr[i] = arr[i-1];
     }
 
-    arr[pos] = x;
+    //insert at position
+    arr[pos] = ele;
     n++;
-    
-    printf("New array: ");
-    for (i = 0; i < n; i++) printf("%d ", arr[i]);
-    
+
+    printf("\nAfter: ");
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+
     return 0;
 }
