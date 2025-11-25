a60ed3c44cf633a3db9f99ba22f97b1f2bc8aefe
Rupesh Kumar <rk0095269@gmail.com>
2025-11-25 22:00:47 +0530

    Update Question_65.c

--- Question_65.c
+++ Question_65.c
@@ -1,33 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int arr[] = {2, 5, 8, 12, 16, 23, 38, 45, 67, 89};
-    int n = 10;
-    int target, low = 0, high = n - 1, mid, found = 0;
+int main()
+{
+    int arr[] = {2,5,8,12,16,23,38,45,67,89};
+    int n=10;
+    int search=23;
+    int low=0,high=n-1,mid;
+    int found=0;
     
-    printf("Enter number to search: ");
-    scanf("%d", &target);
-    
-    while (low <= high) {
-        mid = (low + high) / 2;
+    while(low<=high)
+    {
+        mid=(low+high)/2;
         
-        if (arr[mid] == target) {
-            printf("Found at position %d\n", mid);      // Search in a sorted array using binary search.
-
-            found = 1;
+        if(arr[mid]==search)
+        {
+            found=1;
             break;
         }
-        else if (arr[mid] < target) {
-            low = mid + 1;
+        else if(arr[mid]<search)
+        {
+            low=mid+1;
         }
-        else {
-            high = mid - 1;
+        else
+        {
+            high=mid-1;
         }
     }
     
-    if (!found) {
-        printf("Not found\n");
-    }
-    
+    if(found==1)
+        printf("Element %d found at index %d",search,mid);
+    else
+        printf("Element %d not found",search);
+        
     return 0;
 }
