10db016679b62d366768c9609093e12542ed7de9
Rupesh Kumar <rk0095269@gmail.com>
2025-11-25 22:09:42 +0530

    Update Questio_68.c

--- Questio_68.c
+++ Questio_68.c
@@ -1,23 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int arr[100] = {1, 2, 3, 4, 5};
-    int n = 5; 
-    int pos, i;
+int main()
+{
+    int a[8] = {5,12,8,3,15,7,9,4};
+    int n=8;
+    int del=8;
+    int i,j;
+    int found=0;
     
-    printf("Array: ");
-    for (i = 0; i < n; i++) printf("%d ", arr[i]);     // Delete an element from an array.
-
-    printf("\nEnter position to delete: ");
-    scanf("%d", &pos);
+    printf("Before: ");
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
     
-    for (i = pos; i < n - 1; i++) {
-        arr[i] = arr[i+1];
+    //find element
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==del)
+        {
+            found=1;
+            //shift elements left
+            for(j=i;j<n-1;j++)
+            {
+                a[j]=a[j+1];
+            }
+            n--;
+            break;
+        }
     }
-    n--;
     
-    printf("New array: ");
-    for (i = 0; i < n; i++) printf("%d ", arr[i]);
+    if(found==0)
+        printf("\nElement not found");
+    else
+    {
+        printf("\nAfter: ");
+        for(i=0;i<n;i++)
+            printf("%d ",a[i]);
+    }
     
     return 0;
 }
