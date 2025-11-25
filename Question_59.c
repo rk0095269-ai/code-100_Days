38b7b064ab564fddce0adc0282c9c4641c381534
Rupesh Kumar <rk0095269@gmail.com>
2025-11-25 21:47:27 +0530

    Update Question_59.c

--- Question_59.c
+++ Question_59.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int n, evenCount = 0, oddCount = 0;
-
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 == 0) {
-            evenCount++;
-        } else {
-            oddCount++;
+int main()
+{
+    int numbers[] = {2, 5, 8, 3, 10, 7, 4, 9, 6, 1};
+    int n = 10;
+    int even_count = 0, odd_count = 0;
+    int i;
+    
+    // checking each number
+    for(i=0; i<n; i++)
+    {
+        if(numbers[i] % 2 == 0)
+        {
+            even_count++;  // even number found
+        }
+        else
+        {
+            odd_count++;   // odd number found
         }
     }
-
-    printf("Number of even elements = %d\n", evenCount);
-    printf("Number of odd elements = %d\n", oddCount);
-
+    
+    // printing results
+    printf("Total even numbers: %d\n", even_count);
+    printf("Total odd numbers: %d\n", odd_count);
+    
     return 0;
 }
