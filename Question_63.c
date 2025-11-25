3b1255015dcc37b5fd89a8c27ac1c0f49960b2ae
Rupesh Kumar <rk0095269@gmail.com>
2025-11-25 21:57:17 +0530

    Update Question_63.c

--- Question_63.c
+++ Question_63.c
@@ -1,22 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int arr1[] = {1, 2, 3};
-    int arr2[] = {4, 5, 6};
-    int merged[6];
-                                        // Merge two arrays
-
-    for (int i = 0; i < 6; i++) {
-        if (i < 3) {
-            merged[i] = arr1[i];
-        } else {
-            merged[i] = arr2[i - 3];
-        }
+int main()
+{
+    int a[] = {1,3,5,7};
+    int b[] = {2,4,6,8};
+    int m=4,n=4;
+    int c[8];
+    int i,k=0;
+    
+    //copy first array
+    for(i=0;i<m;i++)
+    {
+        c[k] = a[i];
+        k++;
     }
     
-    printf("Merged: ");
-    for (int i = 0; i < 6; i++) {
-        printf("%d ", merged[i]);
+    //copy second array
+    for(i=0;i<n;i++)
+    {
+        c[k] = b[i];
+        k++;
+    }
+    
+    printf("Merged array: ");
+    for(i=0;i<8;i++)
+    {
+        printf("%d ",c[i]);
     }
     
     return 0;
