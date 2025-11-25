93e6a351d8ff9e534d28a4b43a2d30080792e6cf
Rupesh Kumar <rk0095269@gmail.com>
2025-11-25 21:58:27 +0530

    Update Quetion_64.c

--- Quetion_64.c
+++ Quetion_64.c
@@ -1,24 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int n, d, i, max = 0;
-    int cnt[10] = {0};
+int main()
+{
+    int n=12235555;
+    int count[10]={0};
+    int digit,max=0,most;
     
-    printf("Enter number: ");     // Find the digit that occurs the most times in an integer number.
-
-    scanf("%d", &n);
+    printf("Number: %d\n",n);
     
-    if (n < 0) n = -n;
-    
-    while (n > 0) {
-        cnt[n % 10]++;
-        n /= 10;
+    while(n>0)
+    {
+        digit=n%10;
+        count[digit]++;
+        n=n/10;
     }
     
-    for (i = 1; i < 10; i++) {
-        if (cnt[i] > cnt[max]) max = i;
+    for(int i=0;i<10;i++)
+    {
+        if(count[i]>max)
+        {
+            max=count[i];
+            most=i;
+        }
     }
     
-    printf("Most frequent: %d\n", max);
+    printf("Most frequent digit: %d\n",most);
+    printf("It appears %d times\n",max);
+    
     return 0;
 }
